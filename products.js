c03a764291cd835d0069de4012a7ff621b82779c
Hugo Linhares <hugo.linhares92@gmail.com>
2023-12-13 11:53:40 -0300

    feat: verify of catalogs using js

---
+++ products.js
@@ -0,0 +1,29 @@
+const catalog = require('./catalog.json')
+const catalogo = require('./catalogo.json')
+
+console.log('catalogo', catalogo)
+console.log('catalog', catalog)
+
+const typedProductCatalog = 'laranja'
+const containsCatalog = catalog.A.product
+console.log(containsCatalog, typedProductCatalog)
+
+if(typedProductCatalog.toLowerCase() === containsCatalog.toLowerCase()) {
+    console.log('equal Catalog')
+}
+
+const typedProductCatalogo = 'limao' // must be exactly the same as stored
+const containsCatalogo = catalogo.B
+console.log(typedProductCatalogo, containsCatalogo)
+
+if(containsCatalogo.includes(typedProductCatalogo)) {
+    console.log('equal Catalogo')
+}
+
+// if not equal as stored, this is needed:
+containsCatalogo.forEach(element => {
+    const containsCatalogoToLowerCase = element.toString().toLowerCase()
+    if(containsCatalogoToLowerCase === typedProductCatalogo.toLowerCase()) {
+        console.log('equal Catalogo not the same stored');
+    }
+});
